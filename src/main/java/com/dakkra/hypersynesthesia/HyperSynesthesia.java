5cdf311c5d77ff252765968fc1ea7492747c1516
Chris Soderquist <contact@dakkra.com>
2023-07-22 19:03:30 -0600

    Add lombok as required module
    Add logging to HypverSynesthesia class

--- src/main/java/com/dakkra/hypersynesthesia/HyperSynesthesia.java
+++ src/main/java/com/dakkra/hypersynesthesia/HyperSynesthesia.java
@@ -1,7 +1,32 @@
 package com.dakkra.hypersynesthesia;
 
 import com.avereon.xenon.Mod;
+import lombok.CustomLog;
 
+@CustomLog
 public class HyperSynesthesia extends Mod {
 
+	@Override
+	public void register() {
+		super.register();
+		log.atInfo().log( "Registering HyperSynesthesia");
+	}
+
+	@Override
+	public void startup() throws Exception {
+		super.startup();
+		log.atInfo().log( "Initializing HyperSynesthesia");
+	}
+
+	@Override
+	public void shutdown() throws Exception {
+		super.shutdown();
+		log.atInfo().log( "Closing HyperSynesthesia");
+	}
+
+	@Override
+	public void unregister() {
+		super.unregister();
+		log.atInfo().log( "UnRegistering HyperSynesthesia");
+	}
 }
