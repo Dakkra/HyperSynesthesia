72eefc02bae84c59c853be064a8a7811f7985529
msoderquist <msoderquist@mastercontrol.com>
2024-02-19 18:45:23 -0700

    Fix resource bundle loading

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -7,7 +7,10 @@ module com.dakkra.hypersynthesthesia {
 	requires org.slf4j;
 	requires fft4j;
 
+	opens com.dakkra.hypersynesthesia.bundles;
+	//opens com.dakkra.hypersynesthesia.settings;
+	opens com.dakkra.hypersynesthesia to org.testfx.junit5;
+
 	exports com.dakkra.hypersynesthesia to com.avereon.xenon;
 	provides com.avereon.xenon.Mod with HyperSynesthesia;
-	opens com.dakkra.hypersynesthesia to org.testfx.junit5;
 }
