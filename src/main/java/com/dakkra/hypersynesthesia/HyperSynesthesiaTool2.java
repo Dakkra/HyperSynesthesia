705ef0924d1b2a9dbe96d9f5af4b5d6b7ad02c8f
msoderquist <msoderquist@mastercontrol.com>
2024-02-19 18:53:20 -0700

    Add run/pause tool button

--- src/main/java/com/dakkra/hypersynesthesia/HyperSynesthesiaTool2.java
+++ src/main/java/com/dakkra/hypersynesthesia/HyperSynesthesiaTool2.java
@@ -3,11 +3,30 @@ package com.dakkra.hypersynesthesia;
 import com.avereon.xenon.XenonProgramProduct;
 import com.avereon.xenon.asset.Asset;
 import com.avereon.xenon.tool.guide.GuidedTool;
+import com.avereon.xenon.workpane.ToolException;
 
 public class HyperSynesthesiaTool2 extends GuidedTool {
 
 	public HyperSynesthesiaTool2( XenonProgramProduct product, Asset asset ) {
 		super( product, asset );
+	}
+
+	@Override
+	protected void activate() throws ToolException {
+		super.activate();
+
+		// TODO Register the run/pause action
+
+		pushTools( "runpause" );
+	}
+
+	@Override
+	protected void conceal() throws ToolException {
+		pullTools();
+
+		// TODO Unregister the run/pause action
+
+		super.conceal();
 	}
 
 }
