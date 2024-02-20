c124f85bc232788ffeaf711cff11962b5c1cb196
msoderquist <msoderquist@mastercontrol.com>
2024-02-19 17:33:14 -0700

    Add new tool implementation

---
+++ src/main/java/com/dakkra/hypersynesthesia/HyperSynesthesiaTool2.java
@@ -0,0 +1,13 @@
+package com.dakkra.hypersynesthesia;
+
+import com.avereon.xenon.XenonProgramProduct;
+import com.avereon.xenon.asset.Asset;
+import com.avereon.xenon.tool.guide.GuidedTool;
+
+public class HyperSynesthesiaTool2 extends GuidedTool {
+
+	public HyperSynesthesiaTool2( XenonProgramProduct product, Asset asset ) {
+		super( product, asset );
+	}
+
+}
